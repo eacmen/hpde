fb274c829e1c7f36b92fed7aeb3830d8f5f35b40
Peter Eacmen <peacmen@tacnetsol.com>
2017-10-11 11:13:22 -0400

    Tested and working on the street, but failed to work consistently on track

--- rcp-lua/gopro_session_multicam.lua
+++ rcp-lua/gopro_session_multicam.lua
@@ -1,6 +1,8 @@
 -- This code is derived from the Auto Sport Labs Race Capture Pro wiki and forums
 -- modifications were made to support multiple Hero4 Session Cameras
 
+script_start_time = getUptime( )
+
 STATUS_PREINIT   = 0
 STATUS_INIT_SENT = 1
 STATUS_GOT_IP    = 2
@@ -9,6 +11,7 @@ STATUS_RECORDING = 4
 STATUS_FAIL      = 5
 STATUS_STOPPED   = 6
 
+TRIGGER_INIT       = 0
 TRIGGER_STARTING   = 1
 TRIGGER_STOPPING   = 2
 TRIGGER_STARTED    = 3
@@ -23,17 +26,15 @@ WIFI_SERIAL_PORT = 4
 cameras = {
    { ssid='GoPro1',
      psk='password1',
-     mac="d4d9xxxxxxxx",
+     mac="001122334455",
      status=STATUS_PREINIT,
-     init_time=0,
-     active=1
+     init_time=0
    },
    { ssid='GoPro2',
      psk='password2',
-     mac="d4d9xxxxxxxx",
+     mac="001122334456",
      status=STATUS_PREINIT,
-     init_time=0,
-     active=0
+     init_time=0
    }
 }
 
@@ -41,7 +42,7 @@ cameras = {
 camera_count = 2
 
 -- Set to 1 if testing GoPros in the garage
-bench_test = 1
+bench_test =  0
 
 --Speed threshold to start recording
 START_SPEED = 20
@@ -59,7 +60,7 @@ gopro_id = addChannel("GoPro", 10, 0, 0, 100, "")
 
 gopro_num_recording = 0
 
-trigger_state = TRIGGER_STOPPING
+trigger_state = TRIGGER_INIT
 
 active_cam = 1
 
@@ -69,7 +70,7 @@ active_cam = 1
 -----------------------------
 
 function log(msg)
-   println('[GoProWiFi ' ..tostring(active_cam) ..'] '  ..msg)
+--   println('[GoProWiFi ' ..tostring(active_cam) ..'] '  ..msg)
 end
 
 function send_crlf()
@@ -111,12 +112,12 @@ function send_shutter(cam, cmd)
 end
 
 function start_gopro(cam)
-  log('start GoPro')
+--  log('start GoPro')
   send_shutter(cam, '01')
 end
 
 function stop_gopro(cam)
-   log('stopping GoPro')
+--   log('stopping GoPro')
    send_shutter(cam, '00')
 end
 
@@ -128,8 +129,7 @@ function wake_gopro(cam)
      macChars = macChars .. string.char(tonumber(w, 16)) 
   end
     
-  local magicPacket = string.char(0xff):rep(6) .. macChars:rep(16); 
-  log('Waking GoPro') 
+  local magicPacket = string.char(0xff):rep(6) .. macChars:rep(16);
   send_AT('AT+CIPSTART="UDP","10.5.5.9",9') 
   sleep(500) 
   send_AT('AT+CIPSEND=' ..toInt(#magicPacket)) 
@@ -141,7 +141,7 @@ function wake_gopro(cam)
 end
 
 function init_wifi(cam)
-  log('initializing')
+--  log('initializing')
   send_AT('AT+RST')
   sleep(2000)
   send_AT('AT+CWMODE_CUR=1')
@@ -160,7 +160,7 @@ function process_incoming(cam)
   
    if cam.status == STATUS_GOT_IP and string.match(line, 'OK') then
       cam.status = STATUS_READY
-      log('ready for GoPro')
+--      log('ready for GoPro')
    end
 end
 
@@ -171,22 +171,22 @@ function set_trigger_state( )
    end
 
    if bench_test == 1 then
-      if getUptime( ) < 5000 then
+      if (getUptime( ) - script_start_time) < 5000 then
          trigger_state = TRIGGER_STARTING
       end
 
-      if getUpTime( ) > 120000 then
+      if (getUptime( ) - script_start_time) > 90000 then
          trigger_state = TRIGGER_STOPPING
       end
       return
    else
       local speed = getGpsSpeed( )
 
-      if speed > START_SPEED and bench_test == 0 and trigger_state != TRIGGER_STARTED then
+      if speed > START_SPEED and trigger_state ~= TRIGGER_STARTED then
          trigger_state = TRIGGER_STARTING
       end
 
-      if speed < STOP_SPEED and bench_test == 0 then
+      if speed < STOP_SPEED and trigger_state ~= TRIGGER_INIT then
          trigger_state = TRIGGER_STOPPING
       end
    end
@@ -201,10 +201,16 @@ function check_gopro()
    end
 
    if (cameras[active_cam].status == STATUS_RECORDING or cameras[active_cam].status == STATUS_FAIL or cameras[active_cam].status == STATUS_STOPPED) then
+
+      if trigger_state == TRIGGER_STOPPING and cameras[active_cam].status == STATUS_RECORDING then
+         cameras[active_cam].status = STATUS_READY
+         return
+      end
+      
       -- we need to move to the next camera
       if cameras[active_cam].status == STATUS_FAIL or cameras[active_cam].status == STATUS_STOPPED then
          -- lets try this camera again if we can
-         cameras[active_cam].status == STATUS_PREINIT
+         cameras[active_cam].status = STATUS_PREINIT
       end
 
       active_cam = active_cam + 1
@@ -223,7 +229,7 @@ function check_gopro()
    process_incoming(cameras[active_cam]) -- sets GOT_IP or READY state on camera
       
    if cameras[active_cam].status == STATUS_INIT_SENT and getUptime() > cameras[active_cam].init_time + INIT_TIMEOUT then
-      log('could not connect to GoPro')
+      println('could not connect to GoPro ' ..tostring(active_cam))
       cameras[active_cam].status = STATUS_FAIL
       return
    end
@@ -232,7 +238,7 @@ function check_gopro()
     return
   end
   
-  if cameras[active_cam].status ~= STATUS_RECORDING and trigger_state == TRIGGER_STARTING then
+  if trigger_state == TRIGGER_STARTING then
      wake_gopro(cameras[active_cam])
      start_gopro(cameras[active_cam])
      cameras[active_cam].status = STATUS_RECORDING
@@ -240,8 +246,8 @@ function check_gopro()
      return
   end
 
-  if cameras[active_cam].status == STATUS_RECORDING and trigger_state == TRIGGER_STOPPING then
-     stop_gopro()
+  if trigger_state == TRIGGER_STOPPING then
+     stop_gopro(cameras[active_cam])
      cameras[active_cam].status = STATUS_STOPPED
      gopro_num_recording = (gopro_num_recording - 1)
      return
