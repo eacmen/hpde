500e40d19805c74c6a778cf0b9d1988fb518cd6c
Peter Eacmen <peacmen@refirmlabs.com>
2019-07-30 23:11:29 -0400

    first attempt at math channel generation

--- rcp-python-parser/rcp-parse.py
+++ rcp-python-parser/rcp-parse.py
@@ -5,13 +5,24 @@ import sys
 import csv
 import struct
 
+from sympy import *
+from sympy.parsing.sympy_parser import parse_expr
+
+MATH_CHANNELS = [ ("Math_Roll", -180, 180, "deg", 10, "180 * atan(AccelY/sqrt(AccelX**2 + AccelZ**2))/pi"), ]
+
 
 
 if __name__ == '__main__':
     headings = None
-    outfile = open(sys.argv[2], 'wb')
-    with open(sys.argv[1], 'r') as rcpfile:
-        rcpreader = csv.reader(rcpfile)
+    heading_stats = { }
+    values = [ ]
+    expressions = { }
+    for name,min,max,units,rate,eqn in MATH_CHANNELS:
+        expr = parse_expr(eqn, evaluate=False)
+        expressions[name] = expr
+    with open(sys.argv[1], 'r') as rcpfile, open(sys.argv[2], 'w', newline='') as outfile:
+        rcpwriter = csv.writer(outfile, delimiter = ',', quoting=csv.QUOTE_NONE, quotechar='',escapechar='\\')
+        rcpreader = csv.reader(rcpfile, delimiter = ',', quoting=csv.QUOTE_NONE)
         for row in rcpreader:
             if headings is None:
                 headings = { }
@@ -21,17 +32,49 @@ if __name__ == '__main__':
                     (name, unit, min, max, rate) = vals
                     headings[name] = col
                     col += 1
+                for (name,min,max,units,rate,eqn) in MATH_CHANNELS:
+                    row.append(f'"{name}"|"{units}"|{min}|{max}|{rate}')
+                rcpwriter.writerow(row)
             else:
-                f1 = row[headings['MK20F1']]
-                f2 = row[headings['MK20F2']]
-                speed = row[headings['Speed']]
-                if len(f1) == 0 or len(f2) == 0:
-                    continue
-                #if int(f1) == 3735928559 or int(f2) == 3735928559:
-                #    #fucking deadbeef in CAN really?
-                #    continue
-                
-                outfile.write(struct.pack(">II", int(f1), int(f2)))
-                #print("%d   %d" % (int(f1), int(f2)))
-                print("%08x   %08x" % (int(f1), int(f2)))
-                print("{:032b}   {:032b}".format(int(f1), int(f2)))
+                thisrow = { }
+                for name,col in headings.items( ):
+                    value = row[col]
+                    if len(value) == 0:
+                        continue
+                    value = float(value)
+                    thisrow[name] = value
+                    if name not in heading_stats:
+                        heading_stats[name] = (value, value, value)
+                    else:
+                        (min, max, avg) = heading_stats[name]
+                        if value < min:
+                            min = value
+                        if value > max:
+                            max = value
+                        avg = (avg + value)/2
+                        heading_stats[name] = (min, max, avg)
+                values.append(thisrow)
+                for name,min,max,units,rate,eqn in MATH_CHANNELS:
+                    expr = expressions[name]
+                    try:
+                        result = float(expr.evalf(subs=thisrow))
+                        row.append(f'{result:.2f}')
+                    except TypeError:
+                        row.append('')
+                    rcpwriter.writerow(row)
+                        
+    
+    print("Name\tMin\tMax\tAvg")
+    for name, stats in heading_stats.items( ):
+        (min, max, avg) = stats
+        print(f"{name}\t{min}\t{max}\t{avg}")
+"""
+    for name,min,max,units,rate,eqn in MATH_CHANNELS:
+        expr = parse_expr(eqn, evaluate=False)
+        for row in values:
+            try:
+                result = float(expr.evalf(subs=row))
+                print(result)
+            except TypeError:
+                pass
+"""         
